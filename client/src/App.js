b83fb2460b1f4347e230f3b4a0a4495c11b8f276
Anton <Ashotkupriy06@mail.ru>
2026-05-06 22:14:47 +0500

    Fix client directory as regular folder

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import WebSock from './WebSock';
+// import LongPulling from './LongPulling';
+// import EventSourcing from './EventSourcing';
+
+
+function App() {
+  return (
+    <div className="App">
+      <WebSock/>
+    </div>
+  );
+}
+
+export default App;
