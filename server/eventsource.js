222f92d8d4dfe22356c14be07911721b30cab25c
Anton <Ashotkupriy06@mail.ru>
2026-05-06 21:59:35 +0500

    three realtime chats

---
+++ server/eventsource.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import events from 'events';
+
+const PORT = 5000;
+
+const emitter = new events.EventEmitter();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/connect', ((req, res) => {
+  res.writeHead(200, {
+    'Connection': 'keep-alive',
+    'Content-Type': 'text/event-stream',
+    'Cache-control': 'no-cashe'
+  })
+  emitter.on('newMessage', (message) => {
+    res.write(`data: ${JSON.stringify(message)} \n\n`);
+  })
+}))
+
+app.post('/new-messages', ((req, res) => {
+  const message = req.body;
+  emitter.emit('newMessage', message);
+  res.status(200)
+}))
+
+app.listen(PORT, () => {console.log(`Server started on PORT=${PORT}`)})
